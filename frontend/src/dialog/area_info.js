4c7f9aea25ca665951d103b74ba3cd02ca3f8548
reinisindans <reinis.indans@mailbox.tu-dresden.de>
2019-04-29 15:26:14 +0200

    area_info.js:  Datenaufbereitung fertig, angefangen mit Tabellenformatierung
    TODO: Tabellenformatierung

--- frontend/src/dialog/area_info.js
+++ frontend/src/dialog/area_info.js
@@ -9,17 +9,20 @@ const area_info={
         time:0
     },
 
-    text:{ // Translation
+    text:{ // Translation         ACHTUNG! The translation entry keys have to have the same name as the corresponding table column names ( see function area.info.prepareDataForTable() )
         de:{
             title:"Gebietsprofil",
             time: "Zeitpunkt",
             indicatorValues:"Alle Indikatorwerte für: ",
             download:"Herunterladen als .csv",
             category:"Kategorie",
-            indicator:"Indicator",
+            indicator:"Indikator",
             value:"Wert",
-            relevance:"Aktualität",
+            relevanceYear:"Aktualität",
             comparison:"Wergleich mit: ",
+            germany:"Deutschland",
+            region:"Kreis",
+            difference:"Differenz"
         },
         en:{
             title:"Area information",
@@ -29,24 +32,58 @@ const area_info={
             category:"Category",
             indicator:"Indicator",
             value:"Value",
-            relevance:"Topicality",
-            comparison:"Comparison with: "
+            relevanceYear:"Topicality",
+            comparison:"Comparison with:",
+            germany: "Germany",
+            region: "Kreis",
+            difference:"Difference"
         }
     },
 
     open:function(ags,gen){
         this.parameters=this.getAllParameters(ags, gen); // getting the regular Parameters
+        let columnList=["category","indicator", "value", "relevanceYear","valueBRD", "differenceToBRD"]; // list of data columns that should be displayed in the Table - used to sort the data
+                                                                                                             // -- ACHTUNG!! Has to be the same as the column names defined in function area_info.prepareDataForTAble() !
 
         $.when(RequestManager.getSpatialOverview(indikatorauswahl.getSelectedIndikator(),ags).done(function(data){    // Fetching the data. Async function, waiting for results before continuing
                 data= area_info.prepareDataForTable(data,area_info.parameters.lan);
                 area_info.parameters.data=data;
                 let html= area_info.writeHTML(area_info.parameters,area_info.text);
                 area_info.createDialogWindow(area_info.parameters,html,area_info.text);
-                area_info.drawTable(data,area_info.parameters.lan,area_info.text);
+                area_info.initDropdown(area_info.parameters,area_info.text);
+                area_info.drawTable(data,area_info.parameters.lan,area_info.text,columnList);
             })
         );
 
 
+
+    },
+    initDropdown:function(parameters, text){  // controls the dropdown menu
+        const comparison_dropdown=$("#comparison_ddm");
+        comparison_dropdown.dropdown({
+            onChange: function (value) {
+                let columnList=[];
+                switch (value) {
+                    case "germany":
+
+                        columnList=["category","indicator", "value", "relevanceYear","valueBRD", "differenceToBRD"];
+                        area_info.drawTable(parameters.data,parameters.lan, text, columnList);
+                        comparison_dropdown.dropdown("hide");
+                        console.log("Redrawing table: Germany" );
+                        break;
+
+                    case "region":
+                        columnList=["category","indicator", "value", "relevanceYear","valueKreis", "differenceToKreis"];
+                        area_info.drawTable(parameters.data,parameters.lan, text, columnList);
+                        comparison_dropdown.dropdown("hide");
+                        console.log("Redrawing table: Kreis" );
+                        break;
+                    default:
+                        alert("Error, no chart Type selected!")
+
+                }
+            }
+        })
 
     },
 
@@ -83,9 +120,9 @@ const area_info={
                 }
                 for (let indicator in data[index][category]["values"]){
                     indikatorauswahl.getIndikatorInfo(data[index][category]["values"][indicator]["id"],"name");
-                    indicatorId=data[index][category]["values"][indicator]["id"];
-                    indicatorName="";
-                    indicatorText="";
+                    let indicatorId=data[index][category]["values"][indicator]["id"],
+                        indicatorName="",
+                        indicatorText="";
                     if (lan=="de"){
                         indicatorName=indikatorauswahl.getIndikatorInfo(data[index][category]["values"][indicator]["id"],"ind_name");
                         indicatorText=indikatorauswahl.getIndikatorInfo(data[index][category]["values"][indicator]["id"],"info");
@@ -95,9 +132,9 @@ const area_info={
                         indicatorText=indikatorauswahl.getIndikatorInfo(data[index][category]["values"][indicator]["id"],"info_en");
                     }
                     let tableRow={
-                        categoryName:categoryName,
+                        category:categoryName,
                         id:indicatorId,
-                        indicatorName:indicatorName,
+                        indicator:indicatorName,
                         indicatorText:indicatorText,
                         value:this.roundNumber(indicatorId,data[index][category]["values"][indicator]["value"]), // Value gets rounded based on the Indicator decimal spaces
                         unit:data[index][category]["values"][indicator]["einheit"],
@@ -105,13 +142,13 @@ const area_info={
                         relevanceMonth:data[index][category]["values"][indicator]["grundakt_month"],
                         relevanceYearBRD:data[index][category]["values"][indicator]["grundakt_year_brd"],
                         relevanceMonthBRD:data[index][category]["values"][indicator]["grundakt_month_brd"],
-                        valueBRD:data[index][category]["values"][indicator]["value_brd"],
-                        differenceToBRD:data[index][category]["values"][indicator]["diff_brd"],
+                        valueBRD:this.roundNumber(indicatorId,data[index][category]["values"][indicator]["value_brd"]),  // Value gets rounded based on the Indicator decimal spaces
+                        differenceToBRD:this.roundNumber(indicatorId,data[index][category]["values"][indicator]["diff_brd"]),  // Value gets rounded based on the Indicator decimal spaces
                         relevanceYearKreis:data[index][category]["values"][indicator]["grundakt_year_krs"],
                         relevanceMonthKreis:data[index][category]["values"][indicator]["grundakt_month_krs"],
-                        valueKreis:data[index][category]["values"][indicator]["value_krs"],
-                        differenceToKreis:data[index][category]["values"][indicator]["diff_krs"]};
-                    this.roundNumber(tableRow.id);
+                        valueKreis:this.roundNumber(indicatorId, data[index][category]["values"][indicator]["value_krs"]),
+                        differenceToKreis:this.roundNumber(indicatorId,data[index][category]["values"][indicator]["diff_krs"])
+                    };
                     categoryName=" ";
                     tableData.push(tableRow);
                 }
@@ -123,15 +160,18 @@ const area_info={
 
     },
 
-    // TODO REINI CONTINUE WRITING SELECTING FUNCTION FOR THE  DATA TABLE COLUMNS
-
     selectColumnsForTable:function(data, columnList){
-        newTableColumns=[];
+        let newTableColumns=[];
         for (let elem in data){
-            rowColumns=[];
-            for (let columnName in columnList){}
-            column=data[elem].columnList[columnName]
+            let tableRow=[];
+            for (let columnName in columnList){
+                let column=data[elem][columnList[columnName]];
+                console.log("Got Column: "+ column);
+                tableRow.push(column)
+            }
+            newTableColumns.push(tableRow)
         }
+        return newTableColumns;
     },
 
     writeHTML:function(parameters, text){
@@ -153,7 +193,27 @@ const area_info={
             </div>
             <br/>
             <hr />
-            <table id="dataTable" class="display" width="90%"></table>
+            <table id="dataTable" class="display" width="90%">
+                    <thead>
+                <tr>
+                <th>${text[parameters.lan].category}</th>
+                <th>${text[parameters.lan].indicator}</th>
+                <th>${text[parameters.lan].value}</th>
+                <th>${text[parameters.lan].relevanceYear}</th>
+                <th>${text[parameters.lan].comparison}
+                    <div id="comparison_ddm" class="ui selection dropdown">
+                        <i class="dropdown icon"></i>    
+                        <div class="text">${text[parameters.lan].germany}</div>                    
+                        <div class="menu">
+                           <div class="item" data-value="germany">${text[parameters.lan].germany}</div>
+                           <div class="item" data-value="region">${text[parameters.lan].region}</div>
+                        </div>
+                     </div>
+                </th>
+                <th>${text[parameters.lan].difference}</th>
+                </tr>
+        </thead>
+</table>
         </div>
         `);
     },
@@ -166,18 +226,23 @@ const area_info={
         dialog_manager.create();
     },
 
-    drawTable:function(data, lan, text){
-        console.log("This is our DATA:  "+data);
+    drawTable:function(data, lan, text, columnList){
+        data=area_info.selectColumnsForTable(data,columnList);  // getting only the data required for the Table
+        /*let tableColumns= [];
+        for (let columnName in columnList){     // getting the column Names
+            console.log("Got Column in DrawTable: "+ columnList[columnName]);
+            tableColumns.push({title:text[lan][columnList[columnName]]});
+            console.log("Added Column: "+ text[lan][columnList[columnName]]);
+        }
+        */
         $("#dataTable").DataTable(
             {
+                destroy:true,
                 data:data,
-                columns:[
-                    { title: text[lan].category },
-                    { title: text[lan].indicator},
-                    { title: text[lan].value},
-                    { title: text[lan].relevance},
-                    { title: text[lan].comparison}
-                ]
+                "ordering": false,  // disable the ordering of columns
+                "createdRow": function ( row, data, index ) {   // formatting the row at draw time! difference Row @ runtime
+                }
+
             }
         )
     },
